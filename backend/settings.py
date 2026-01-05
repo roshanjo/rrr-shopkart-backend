3a4c7ee3b95b228c48aa0a38be2f46ab81d41dc4
roshanjo <roshanvalloppillil@gmail.com>
2026-01-05 16:01:11 +0530

    Allow Render hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5%pn$m%^7cv96lfr26==ceht!$-si1llv(g%4xn9g758l_zenk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
