d471ea948f9596396f5ec3089f0221e1880ab3bc
roshanjo <roshanvalloppillil@gmail.com>
2026-01-08 14:13:53 +0530

    Connect root URL to api app

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import signup, login, test_api, home
 
 urlpatterns = [
-    path("", home),            # root /
+    path("admin/", admin.site.urls),
+    path("", include("api.urls")),           
     path("signup/", signup),
     path("login/", login),
     path("test/", test_api),
