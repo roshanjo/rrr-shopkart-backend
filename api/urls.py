6c18f4b62ae8e7661e9702042311005e796dd2a7
roshanjo <roshanvalloppillil@gmail.com>
2026-01-08 14:06:14 +0530

    Add root route health check

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup, login, test_api
+from .views import signup, login, test_api, home
 
 urlpatterns = [
+    path("", home),            # root /
     path("signup/", signup),
     path("login/", login),
     path("test/", test_api),
