5c8e7f0284845183c6e28f5990830381b5a31c91
roshanjo <roshanvalloppillil@gmail.com>
2026-01-08 14:26:39 +0530

    Fix project-level Django URL routing

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import signup, login, test_api, home
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("api.urls")),           
+    path("", home),
     path("signup/", signup),
     path("login/", login),
     path("test/", test_api),
