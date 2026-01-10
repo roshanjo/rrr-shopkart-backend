be90f1c6749e2aa280a004306badff24fb43c59e
roshanjo <roshanvalloppillil@gmail.com>
2026-01-11 02:27:12 +0530

    Add gunicorn for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 # --------------------------------------------------
 # URL / WSGI
 # --------------------------------------------------
