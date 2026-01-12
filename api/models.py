81df4f26de375f6777061c815ceb318a04b0b020
roshanjo <roshanvalloppillil@gmail.com>
2026-01-12 19:05:00 +0530

    fixxx

--- api/models.py
+++ api/models.py
@@ -17,5 +17,7 @@ class Order(models.Model):
     total = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     stripe_session_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"Order {self.id}"
