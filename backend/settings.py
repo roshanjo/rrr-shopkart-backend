8785be5418b99917b0ac1f27f2f7a5e5b22012be
roshanjo <roshanvalloppillil@gmail.com>
2026-01-12 16:31:50 +0530

    Fix middleware order for production

--- backend/settings.py
+++ backend/settings.py
@@ -42,9 +42,10 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # --------------------------------------------------
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 # --------------------------------------------------
 # TEMPLATES (REQUIRED FOR ADMIN)
